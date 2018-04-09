e56e58334477c81f5249c02434e8d487987f646e
Remy Wang <remywang@uw.edu>
2018-04-09 16:16:19 -0700

    fix run

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
 
 N=4
-for j in $( find $1 -name *.java ); do
+for j in $( find $1 -name "*.java" ); do
   ((i=i%N)); ((i++==0)) && wait
   stack exec joogle-exe "$j" &
 done
