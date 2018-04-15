4dbe5bd8e6f639ed8ff0ef8eb2c73d1c7d606241
Remy Wang <remywang@uw.edu>
2018-04-15 12:30:28 -0700

    fix bash scripts

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
 
 N=4
-for j in $( find $1 -name *.java ); do
+for j in $( find $1 -name "*.java" ); do
   echo "$j"; stack exec joogle-exe "$j" | grep -o haha ; stack exec joogle-exe "$j" | grep -o labeled
 done
 
